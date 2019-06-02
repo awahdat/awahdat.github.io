fe9b773328e5402344930a74b7a6102156e34553
awahdat <51273010+awahdat@users.noreply.github.com>
2019-06-01 20:29:21 -0400

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,5 @@
+var dropdown_options = [ 
+{ value: "dmgHurricane",
+  text: "Hurricane Total Damage Cost (in millios of Dollars): 2005-2016" },
+{ value: "frqHurricane",
+  text: "Hurricane Total Events: 2005-2016" }]
