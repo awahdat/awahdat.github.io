81851625de89c4a5422bdf4704bf8311df06cb34
awahdat <51273010+awahdat@users.noreply.github.com>
2019-06-14 13:20:23 -0400

    Delete dropdown.js

--- dropdown.js
+++
@@ -1,5 +0,0 @@
-var dropdown_options = [ 
-{ value: "dmgHurricane",
-  text: "Hurricane Total Damage Cost (in millios of Dollars): 2005-2016" },
-{ value: "frqHurricane",
-  text: "Hurricane Total Events: 2005-2016" }]
